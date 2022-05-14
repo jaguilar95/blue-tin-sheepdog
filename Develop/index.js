e7b3f2e610f36c557d112b821ce14d65447c0438
Leonardo <josue.aguilar1995@gmail.com>
2022-05-14 11:15:21 -0500

    wrapped up initial questions

--- Develop/index.js
+++ Develop/index.js
@@ -34,20 +34,130 @@ const questions = [
   },
 
   // installation instruction
+  {
+    type: "input",
+    name: "installation",
+    message: "Enter in instructions on how to install your program. (Required)",
+    validate: (installationInput) => {
+      if (installationInput) {
+        return true;
+      } else {
+        console.log("Please enter the project's installation instructions!");
+        return false;
+      }
+    },
+  },
 
   // usage information
+  {
+    type: "input",
+    name: "usage",
+    message: "Enter in instructions and examples for use. (Required)",
+    validate: (usageInput) => {
+      if (usageInput) {
+        return true;
+      } else {
+        console.log("Please enter in a set of instructions for your project!");
+      }
+    },
+  },
 
   // contribution guidelines
+  {
+    type: "confirm",
+    name: "contributionsConfirm",
+    message: "Would you like to add a 'How to Contribute' section?",
+    default: false,
+  },
+  {
+    type: "input",
+    name: "contributions",
+    message:
+      "Please enter a brief description on how you would like users to contribute.",
+    when: ({ contributionsConfirm }) => {
+      if (contributionsConfirm) {
+        return true;
+      } else {
+        return false;
+      }
+    },
+  },
 
   // test instruction
+  {
+    type: "confirm",
+    name: "testConfirm",
+    message: 'Would you like to add a "Tests" section?',
+    default: false,
+  },
+  {
+    type: "input",
+    name: "tests",
+    message: "Please enter in a description of the tests for your project.",
+    when: ({ testConfirm }) => {
+      if (testConfirm) {
+        return true;
+      } else {
+        return false;
+      }
+    },
+  },
 
   // license (list)
+  {
+    type: "checkbox",
+    name: "license",
+    message:
+      "Which licenses would you like to select for your project? (Check any that apply)",
+    choices: [
+      "MIT",
+      "GNU GPLv3",
+      "GNU AGPLv3",
+      "GNU LGPLv3",
+      "Mozilla Public 2.0",
+      "Apache 2.0",
+      "Boost Software 1.0",
+      "Unlicense",
+    ],
+  },
 
   // GitHub username
+  {
+    type: "input",
+    name: "userName",
+    message: "What is your GitHub username password? (Required)",
+    validate: (userNameInput) => {
+      if (userNameInput) {
+        return true;
+      } else {
+        console.log("Please enter your GitHub username!");
+        return false;
+      }
+    },
+  },
 
   // email address
+  {
+    type: "input",
+    name: "email",
+    message: "What email address should users contact you by? (Required)",
+    validate: (emailInput) => {
+      if (emailInput) {
+        return true;
+      } else {
+        console.log("Please enter in an email address!");
+        return false;
+      }
+    },
+  },
 
   // table of contents
+  {
+    type: "confirm",
+    name: "table",
+    message: "Would you like to include a table of contents?",
+    default: true,
+  },
 ];
 
 const promptUser = () => {
