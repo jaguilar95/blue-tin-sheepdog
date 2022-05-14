f6b2c9d92aa0311e996761894ad76f036ee82164
Leonardo <josue.aguilar1995@gmail.com>
2022-05-14 10:15:50 -0500

    initial questions

--- Develop/index.js
+++ Develop/index.js
@@ -1,13 +1,70 @@
 // TODO: Include packages needed for this application
+const inquirer = require("inquirer");
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+  // project title
+  {
+    type: "input",
+    name: "project",
+    message: "What is your project's name? (Required)",
+    validate: (projectInput) => {
+      if (projectInput) {
+        return true;
+      } else {
+        console.log("Please enter your name!");
+        return false;
+      }
+    },
+  },
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+  // description
+  {
+    type: "input",
+    name: "description",
+    message: "Enter in a brief description of your project. (Required)",
+    validate: (descriptionInput) => {
+      if (descriptionInput) {
+        return true;
+      } else {
+        console.log("Please enter a project description!");
+        return false;
+      }
+    },
+  },
 
-// TODO: Create a function to initialize app
-function init() {}
+  // installation instruction
 
-// Function call to initialize app
-init();
+  // usage information
+
+  // contribution guidelines
+
+  // test instruction
+
+  // license (list)
+
+  // GitHub username
+
+  // email address
+
+  // table of contents
+];
+
+const promptUser = () => {
+  return inquirer.prompt(questions);
+};
+
+promptUser()
+  .then((answers) => console.log(answers))
+  .catch((err) => {
+    console.log(err);
+  });
+
+// // TODO: Create a function to write README file
+// function writeToFile(fileName, data) {}
+
+// // TODO: Create a function to initialize app
+// function init() {}
+
+// // Function call to initialize app
+// init();
